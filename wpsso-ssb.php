39d218f97f0cf68648f88bd853c14d612ada7917
JS Morisset <jsm@surniaulula.com>
2015-09-14 13:52:19 -0400

    change minimum required wp version from 3.0 to 3.1

--- wpsso-ssb.php
+++ wpsso-ssb.php
@@ -7,7 +7,7 @@
  * License: GPLv3
  * License URI: http://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO extension to add Social Sharing Buttons with support for hashtags, short URLs, bbPress, BuddyPress, WooCommerce, and much more.
- * Requires At Least: 3.0
+ * Requires At Least: 3.1
  * Tested Up To: 4.3
  * Version: 1.6.8
  * 
