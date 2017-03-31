df57c276d8d5108d3110f1765ec3eca81c7bfd98
JS Morisset <jsm@surniaulula.com>
2017-03-31 10:47:35 +0200

    v2.3.13-1

--- lib/config.php
+++ lib/config.php
@@ -15,7 +15,7 @@ if ( ! class_exists( 'WpssoSsbConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssossb' => array(
-					'version' => '2.3.12-rc1',	// plugin version
+					'version' => '2.3.12-1',	// plugin version
 					'opt_version' => '15',		// increment when changing default options
 					'short' => 'WPSSO SSB',		// short plugin name
 					'name' => 'WPSSO Social Sharing Buttons (WPSSO SSB)',
@@ -28,7 +28,7 @@ if ( ! class_exists( 'WpssoSsbConfig' ) ) {
 					'req' => array(
 						'short' => 'WPSSO',
 						'name' => 'WordPress Social Sharing Optimization (WPSSO)',
-						'min_version' => '3.40.7-rc1',
+						'min_version' => '3.40.7-1',
 					),
 					'img' => array(
 						'icon_small' => 'images/icon-128x128.png',
