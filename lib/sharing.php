d8592adba02a600bdb80fed337aeff893b4dfe2c
JS Morisset <jsm@surniaulula.com>
2017-03-31 13:27:51 +0200

    v2.3.13-1

--- lib/sharing.php
+++ lib/sharing.php
@@ -216,10 +216,9 @@ jQuery("#wpsso-ssb-sidebar-header").click( function(){
 		}
 
 		public function filter_option_type( $type, $key ) {
-
-			if ( ! empty( $type ) )
+			if ( ! empty( $type ) ) {
 				return $type;
-
+			}
 			switch ( $key ) {
 				// integer options that must be 1 or more (not zero)
 				case 'stumble_badge':
