ac7ec807f936c5fcf39db90e147827c45c1d97a5
JS Morisset <jsm@surniaulula.com>
2017-10-04 12:32:45 -0400

    v2.4.7-dev.2

--- lib/shortcode/sharing.php
+++ lib/shortcode/sharing.php
@@ -50,22 +50,30 @@ if ( ! class_exists( 'WpssoSsbShortcodeSharing' ) ) {
 
 		public function add_shortcode() {
 			if ( ! empty( $this->p->options['plugin_shortcodes'] ) ) {
-        			add_shortcode( WPSSOSSB_SHARING_SHORTCODE_NAME, array( &$this, 'do_shortcode' ) );
-				if ( $this->p->debug->enabled ) {
-					$this->p->debug->log( '['.WPSSOSSB_SHARING_SHORTCODE_NAME.'] sharing shortcode added' );
+				if ( ! shortcode_exists( WPSSOSSB_SHARING_SHORTCODE_NAME ) ) {
+        				add_shortcode( WPSSOSSB_SHARING_SHORTCODE_NAME, array( &$this, 'do_shortcode' ) );
+					if ( $this->p->debug->enabled ) {
+						$this->p->debug->log( '['.WPSSOSSB_SHARING_SHORTCODE_NAME.'] sharing shortcode added' );
+					}
+					return true;
+				} elseif ( $this->p->debug->enabled ) {
+					$this->p->debug->log( 'cannot add ['.WPSSOSSB_SHARING_SHORTCODE_NAME.'] sharing shortcode - shortcode already exists' );
 				}
-				return true;
 			}
 			return false;
 		}
 
 		public function remove_shortcode() {
 			if ( ! empty( $this->p->options['plugin_shortcodes'] ) ) {
-				remove_shortcode( WPSSOSSB_SHARING_SHORTCODE_NAME );
-				if ( $this->p->debug->enabled ) {
-					$this->p->debug->log( '['.WPSSOSSB_SHARING_SHORTCODE_NAME.'] sharing shortcode removed' );
+				if ( shortcode_exists( WPSSOSSB_SHARING_SHORTCODE_NAME ) ) {
+					remove_shortcode( WPSSOSSB_SHARING_SHORTCODE_NAME );
+					if ( $this->p->debug->enabled ) {
+						$this->p->debug->log( '['.WPSSOSSB_SHARING_SHORTCODE_NAME.'] sharing shortcode removed' );
+					}
+					return true;
+				} elseif ( $this->p->debug->enabled ) {
+					$this->p->debug->log( 'cannot remove ['.WPSSOSSB_SHARING_SHORTCODE_NAME.'] sharing shortcode - shortcode does not exist' );
 				}
-				return true;
 			}
 			return false;
 		}
