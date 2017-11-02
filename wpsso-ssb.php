a21429a6242c6f5f43d6336d74507a21a62219e2
JS Morisset <jsm@surniaulula.com>
2017-11-02 08:05:18 -0600

    v2.4.8-rc.1

--- wpsso-ssb.php
+++ wpsso-ssb.php
@@ -15,7 +15,7 @@
  * Requires At Least: 3.7
  * Tested Up To: 4.8.3
  * WC Tested Up To: 3.2.2
- * Version: 2.4.8-b.1
+ * Version: 2.4.8-rc.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
