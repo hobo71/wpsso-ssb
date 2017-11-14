eace55cb0d948a153d281b7c321f98541c40c58b
JS Morisset <jsm@surniaulula.com>
2017-11-14 10:57:59 -0600

    v2.5.0

--- wpsso-ssb.php
+++ wpsso-ssb.php
@@ -11,8 +11,8 @@
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO Core extension to add Social Sharing Buttons with support for hashtags, short URLs, bbPress, BuddyPress, WooCommerce, and much more.
- * Requires PHP: 5.3
- * Requires At Least: 3.7
+ * Requires PHP: 5.4
+ * Requires At Least: 3.8
  * Tested Up To: 4.9
  * WC Tested Up To: 3.2.3
  * Version: 2.5.0
