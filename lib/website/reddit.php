90470fd3b08b528438b91c1a90be3d00f8252269
JS Morisset <jsm@surniaulula.com>
2017-12-30 19:20:02 -0600

    v2.5.2

--- lib/website/reddit.php
+++ lib/website/reddit.php
@@ -113,7 +113,7 @@ if ( ! class_exists( 'WpssoSsbWebsiteReddit' ) ) {
 			$js_url = $this->p->ssb_sharing->get_social_file_cache_url( apply_filters( $this->p->cf['lca'].'_js_url_reddit', $js_url, '' ) );
 
 			$html = '<!-- Reddit Button -->'.
-			'<script type="text/javascript">reddit_url="'.esc_url( $atts['url'] ).'"; reddit_title="'.esc_attr( $atts['title'] ).'";</script>'.
+			'<script type="text/javascript">reddit_url="'.esc_url_raw( $atts['url'] ).'"; reddit_title="'.esc_attr( $atts['title'] ).'";</script>'.
 			'<div '.SucomUtil::get_atts_css_attr( $atts, 'reddit' ).'>'.
 			'<script type="text/javascript" src="'.$js_url.'"></script></div>';
 
