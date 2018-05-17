d694374940e9fb37350a2164005d3ede710726c2
JS Morisset <jsm@surniaulula.com>
2018-05-17 16:16:50 -0400

    v2.6.5

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoSsbConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssossb' => array(			// Plugin acronym.
-					'version' => '2.6.4',		// Plugin version.
+					'version' => '2.6.5',		// Plugin version.
 					'opt_version' => '16',		// Increment when changing default option values.
 					'short' => 'WPSSO SSB',		// Short plugin name.
 					'name' => 'WPSSO Social Sharing Buttons',
