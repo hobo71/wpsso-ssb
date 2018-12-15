d5a378eb5b1c89d0033da3d91487eb148bc712e0
JS Morisset <jsm@surniaulula.com>
2018-12-15 17:47:45 -0600

    v2.10.0

---
+++ lib/gettext-pro.php
@@ -0,0 +1,28 @@
+<?php die( 'These aren\'t the droids you\'re looking for...' );
+
+/**
+ * Extract of translatable text strings from the lib/pro classes.
+ */
+_x( 'Sharing Buttons HTML Cache Expiry', 'option label', 'wpsso-ssb' );
+_x( 'seconds (0 to disable)', 'option comment', 'wpsso-ssb' );
+_x( 'Get Social JS Files Cache Expiry', 'option label', 'wpsso-ssb' );
+_x( 'seconds (0 to disable)', 'option comment', 'wpsso-ssb' );
+_x( 'Include on Post Types', 'option label', 'wpsso-ssb' );
+_x( '%s Preset', 'option label', 'wpsso-ssb' );
+_x( 'Force Protocol for Shared URLs', 'option label', 'wpsso-ssb' );
+_x( 'Sharing Buttons HTML Cache Expiry', 'option label', 'wpsso-ssb' );
+_x( 'seconds (0 to disable)', 'option comment', 'wpsso-ssb' );
+_x( 'Get Social JS Files Cache Expiry', 'option label', 'wpsso-ssb' );
+_x( 'seconds (0 to disable)', 'option comment', 'wpsso-ssb' );
+__( 'Save a draft version or publish the %s to display these options.', 'wpsso-ssb' );
+_x( 'Disable Sharing Buttons', 'option label', 'wpsso-ssb' );
+_x( 'Email Subject', 'option label', 'wpsso-ssb' );
+_x( 'Email Message', 'option label', 'wpsso-ssb' );
+_x( 'Tweet Text', 'option label', 'wpsso-ssb' );
+_x( 'Pinterest Caption Text', 'option label', 'wpsso-ssb' );
+_x( 'Tumblr Image Caption', 'option label', 'wpsso-ssb' );
+__( 'Caption disabled - no suitable image found for the %s button', 'wpsso-ssb' );
+_x( 'Tumblr Video Caption', 'option label', 'wpsso-ssb' );
+__( 'Caption disabled - no suitable video found for the %s button', 'wpsso-ssb' );
+_x( 'Position in Woo Short Text', 'option label', 'wpsso-ssb' );
+_x( 'Position in bbPress Single', 'option label', 'wpsso-ssb' );
