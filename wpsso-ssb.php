a781ab45365b9e6491f9152dce4acdc2dc1803ac
JS Morisset <jsm@surniaulula.com>
2019-01-30 08:47:31 -0600

    v2.10.3

--- wpsso-ssb.php
+++ wpsso-ssb.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: WPSSO Social Sharing Buttons [Add-on]
+ * Plugin Name: WPSSO Social Sharing Buttons
  * Plugin Slug: wpsso-ssb
  * Text Domain: wpsso-ssb
  * Domain Path: /languages
