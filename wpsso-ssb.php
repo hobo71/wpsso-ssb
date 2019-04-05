d83f567ad7b2391383804e3e38639e09c3777535
JS Morisset <jsm@surniaulula.com>
2019-04-05 14:47:09 -0600

    v2.11.2

--- wpsso-ssb.php
+++ wpsso-ssb.php
@@ -12,7 +12,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO Core add-on offers social sharing buttons with support for hashtags, short URLs, bbPress, BuddyPress, WooCommerce, and much more.
  * Requires At Least: 3.8
- * Tested Up To: 5.1
+ * Tested Up To: 5.1.1
  * WC Tested Up To: 3.5
  * Version: 2.11.2
  * 
